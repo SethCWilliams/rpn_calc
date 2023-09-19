ebbae43d01794a9dceb6287325fc308ca4f8a003
Seth Williams <fullstackseth@gmail.com>
2023-09-19 15:03:07 -0400

    add cleaner error handling and handle operators first to make ZeroDivisionError cleaner

--- python_version/main.py
+++ python_version/main.py
@@ -1,5 +1,6 @@
 import operator
 
+
 OPERATORS = {
     "+": operator.add,
     "-": operator.sub,
@@ -13,32 +14,39 @@ def rpn_calculator():
 
     while True:
         expression = input("Enter operand(s) and/or operator(s) (q to quit): ")
-
         if expression.lower() == "q":
             break
 
         tokens = expression.split()
 
         for token in tokens:
-            try:
-                if token.isdigit() or (token[0] == "-" and token[1:].isdigit()):
-                    stack.append(float(token))
-                elif token in OPERATORS:
-                    if len(stack) < 2:
-                        print(
-                            "Error: Not enough operands for operator '{}'".format(token)
-                        )
-                        continue
-                    operand2, operand1 = stack.pop(), stack.pop()
+            if token in OPERATORS:
+                if len(stack) < 2:
+                    print(f"Error: Not enough operands for operator '{token}'")
+                    continue
+                operand2, operand1 = stack.pop(), stack.pop()
+                try:
                     result = OPERATORS[token](operand1, operand2)
                     stack.append(result)
-                else:
-                    print("Error: Invalid input '{}'".format(token))
-            except Exception as e:
-                print("Error:", e)
-        if len(stack) > 0:
-            print("Current Result:", stack[-1])
+                except ZeroDivisionError:
+                    print("Error: Division by zero")
+                    stack.append(operand1)
+                    stack.append(operand2)
+            else:
+                try:
+                    # Try converting the token to a float
+                    value = float(token)
+                    stack.append(value)
+                except ValueError:
+                    print(f"Error: Invalid input '{token}'")
+                except Exception as e:
+                    print("Error:", e)
 
+        if stack:
+            print("Current Stack:", stack)
+
+    if stack:
+        print("Final Stack:", stack)
     print("Calculator has been terminated.")
 
 
